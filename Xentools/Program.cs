11ee24e462e99efe67028ad13d1526b86479ba73
Starfer <ivan-on@yandex.ru>
2018-08-12 19:54:47 +0400

    snaplist command

--- Xentools/Program.cs
+++ Xentools/Program.cs
@@ -15,9 +15,33 @@ namespace Xentools
         connect [ip] [port]. Connect to XEN Server. ip - destination ip address, port - destination port
         connect [ip]. Connect to XEN Server. Port is default (443)
         vmlist. Get list of virtual machines.
+        snaplist. Get list of all snapshots.
         disconnect
         exit
         ";
+
+        /// <summary>
+        /// Get list of VMs
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="snapshots">return snapshots too</param>
+        /// <param name="templates">return templates too</param>
+        /// <returns></returns>
+        static List<VM> GetVMs(Session session, bool snapshots = false, bool templates = false)
+        {
+            List<VM> result = new List<VM>();
+            List<XenRef<VM>> vms = new List<XenRef<VM>>();
+            vms = VM.get_all(session);
+            for (int i = 0; i < vms.Count; i++)
+            {
+                VM a = VM.get_record(session, vms[i].opaque_ref);
+                if (a.is_a_snapshot && snapshots) result.Add(a);
+                else if (a.is_a_template && templates) result.Add(a);
+                else if (!a.is_a_template && !a.is_a_snapshot) result.Add(a);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
@@ -80,13 +104,26 @@ namespace Xentools
                     case "vmlist":
                         if (session != null)
                         {
-                            List<XenRef<VM>> vms = new List<XenRef<VM>>();
-                            vms = VM.get_all(session);
-                            for (int i = 0; i < vms.Count; i++)
+                            List<VM> vms = GetVMs(session);
+                            foreach (var vm in vms)
+                                System.Console.WriteLine(vm.name_label);
+                        }
+                        else
+                            System.Console.WriteLine("Not connected");
+                        break;
+                    case "snaplist":
+                        if (session != null)
+                        {
+                            List<VM> vms = GetVMs(session);
+                            foreach (var vm in vms)
                             {
-                                VM a = VM.get_record(session, vms[i].opaque_ref);
-                                if (!a.is_a_snapshot && !a.is_a_template)
-                                    System.Console.WriteLine("{0}",i,a.name_label);
+                                System.Console.WriteLine(vm.name_label);
+                                List<XenRef<VM>> snaplist = vm.snapshots;
+                                foreach (var snapshot in snaplist)
+                                {
+                                    VM snap = VM.get_record(session, snapshot.opaque_ref);
+                                    System.Console.WriteLine("      {0}", snap.name_label);
+                                }
                             }
                         }
                         else
